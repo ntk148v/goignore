96506692987a7df58985caa0b147d8c6b19f2105
Kien Nguyen <kiennt2609@gmail.com>
2023-04-06 15:31:14 +0700

    refactor: remove useless keys

--- goignore.go
+++ goignore.go
@@ -54,39 +54,14 @@ func (i item) Description() string { return i.description }
 func (i item) FilterValue() string { return i.title }
 
 type listKeyMap struct {
-	toggleSpinner       key.Binding
-	toggleTitleBar      key.Binding
-	toggleStatusBar     key.Binding
-	togglePagination    key.Binding
-	toggleHelpMenu      key.Binding
-	pullTemplateUpdates key.Binding
+	updateTemplates key.Binding
 }
 
 func newListKeyMap() *listKeyMap {
 	return &listKeyMap{
-		pullTemplateUpdates: key.NewBinding(
+		updateTemplates: key.NewBinding(
 			key.WithKeys("u"),
-			key.WithHelp("u", "pull the newest gitignore templates"),
-		),
-		toggleSpinner: key.NewBinding(
-			key.WithKeys("s"),
-			key.WithHelp("s", "toggle spinner"),
-		),
-		toggleTitleBar: key.NewBinding(
-			key.WithKeys("T"),
-			key.WithHelp("T", "toggle title"),
-		),
-		toggleStatusBar: key.NewBinding(
-			key.WithKeys("S"),
-			key.WithHelp("S", "toggle status"),
-		),
-		togglePagination: key.NewBinding(
-			key.WithKeys("P"),
-			key.WithHelp("P", "toggle pagination"),
-		),
-		toggleHelpMenu: key.NewBinding(
-			key.WithKeys("H"),
-			key.WithHelp("H", "toggle help"),
+			key.WithHelp("u", "update gitignore templates"),
 		),
 	}
 }
@@ -114,14 +89,14 @@ func newModel() model {
 	ignoreList := list.NewModel(items, delegate, 0, 0)
 	ignoreList.Title = "Ignore templates"
 	ignoreList.Styles.Title = titleStyle
+	ignoreList.AdditionalShortHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			listKeys.updateTemplates,
+		}
+	}
 	ignoreList.AdditionalFullHelpKeys = func() []key.Binding {
 		return []key.Binding{
-			listKeys.toggleSpinner,
-			listKeys.pullTemplateUpdates,
-			listKeys.toggleTitleBar,
-			listKeys.toggleStatusBar,
-			listKeys.togglePagination,
-			listKeys.toggleHelpMenu,
+			listKeys.updateTemplates,
 		}
 	}
 
@@ -150,30 +125,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 		switch {
-		case key.Matches(msg, m.keys.toggleSpinner):
-			cmd := m.list.ToggleSpinner()
-			return m, cmd
-
-		case key.Matches(msg, m.keys.toggleTitleBar):
-			v := !m.list.ShowTitle()
-			m.list.SetShowTitle(v)
-			m.list.SetShowFilter(v)
-			m.list.SetFilteringEnabled(v)
-			return m, nil
-
-		case key.Matches(msg, m.keys.toggleStatusBar):
-			m.list.SetShowStatusBar(!m.list.ShowStatusBar())
-			return m, nil
-
-		case key.Matches(msg, m.keys.togglePagination):
-			m.list.SetShowPagination(!m.list.ShowPagination())
-			return m, nil
-
-		case key.Matches(msg, m.keys.toggleHelpMenu):
-			m.list.SetShowHelp(!m.list.ShowHelp())
-			return m, nil
-
-		case key.Matches(msg, m.keys.pullTemplateUpdates):
+		case key.Matches(msg, m.keys.updateTemplates):
 			cmd := m.list.NewStatusMessage(statusMessageStyle("Pull newest templates"))
 			if err := pullTemplateUpdates(); err != nil {
 				cmd = m.list.NewStatusMessage(errorMessageStyle(err.Error()))
